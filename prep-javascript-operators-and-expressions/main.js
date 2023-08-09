02380b4c69c597acd848e4cfc0af1f0711071c7e
Daniel Perez <danielperezf98@gmail.com>
2023-08-09 03:38:12 +0000

    Completed Javascript Operators and Expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+const number1 = 2;
+const number2 = 4;
+const product = number1 * number2;
+console.log('Value of Product:', product);
+console.log('Type of Product:', typeof product);
+
+const charge = 10;
+const payment = 5;
+const amountRemaining = payment - charge;
+console.log('Value of Amount Remaining:', amountRemaining);
+console.log('Type of Amount Remaining:', typeof amountRemaining);
+
+const tests = 4;
+const assignments = 2;
+const final = 8;
+const grade = (tests + assignments + final) / 3;
+console.log('Value of Grade:', grade);
+console.log('Type of Grade:', typeof grade);
+
+const firstName = 'Daniel';
+const lastName = 'Perez';
+const fullName = firstName + ' ' + lastName;
+console.log('Value of Full Name:', fullName);
+console.log('Type of Full Name:', typeof fullName);
+
+const pH = 4;
+const isAcidic = pH < 7;
+console.log('Value of Is Acidic:', isAcidic);
+console.log('Type of Is Acidic:', typeof isAcidic);
+
+const numberOfSoldiers = 300;
+const isSparta = numberOfSoldiers === 300;
+console.log('Value of Is Sparta:', isSparta);
+console.log('Type of Is Sparta:', typeof isSparta);
+
+const nickname = fullName + ' is the goat!!!';
+console.log('Value of Nickname:', nickname);
+console.log('Type of Nickname:', typeof nickname);
